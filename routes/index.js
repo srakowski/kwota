c5c2990ab59e30ec0c87af49b20add9360b62d4b
srakowski <srakowski@live.com>
2016-03-08 19:50:15 -0600

    start adding quotas

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 var User = require("../models/user");
+var System = require('../models/system');
+var Quota = require('../models/quota');
 
 router.get('/', function(req, res, next) {
     if (req.isAuthenticated()) {
-        res.render('home', {});
+        System.find({ _owner: req.user._id }, function (err, systems) {
+            Quota.find({ _owner: req.user._id }, function (err, quotas) {
+                res.render('home', { systems: systems, quotas: quotas });
+            });          
+        });
     } else {
         res.render('index', {});
     }
