ae3935e22ab0da61bf508eddec28285d31eaed1a
Shawn Rakowski <Shawn Rakowski>
2016-03-10 20:34:21 -0600

    firm up sign in

--- routes/sign.js
+++ routes/sign.js
@@ -5,21 +5,28 @@ var User = require("../models/user");
 var validator = require('validator');
 
 router.get("/up", function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
+        
     res.render('sign/up');        
 });
 
 router.post("/up", function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
+    
     var email = validator.trim(req.body.email);    
     if (!validator.isEmail(email)) {
-        res.render('sign/up', { error: "Invalid email address"});
-        return;
+        return res.render('sign/up', { error: "Invalid email address"});        
     }
                            
     User.register(new User({ email: req.body.email }), req.body.password, function (err) {
         if (err) {
-            res.render('sign/up', { error: err.message});
-            return;
+            return res.render('sign/up', { error: err.message});            
         }        
+        
         passport.authenticate('local')(req, res, function () {
           res.redirect('/');
         });        
@@ -27,13 +34,34 @@ router.post("/up", function (req, res, next) {
 });
 
 router.get("/in", function (req, res, next) {
-    res.render("sign/in", {});
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
+    
+    res.render("sign/in");
 });
 
-router.post("/in", passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/sign/in' 
-}));
+router.post('/in', function(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
+    
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return res.render('sign/in', { error: err.message});
+        }
+        
+        if (!user) { 
+            return res.render('sign/in', { error: "Invalid email/password"}); 
+        }
+        
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.redirect('/');
+        });
+        
+    })(req, res, next);
+});
 
 router.get("/out",
     function (req, res) {    
